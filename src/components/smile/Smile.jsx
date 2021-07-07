0aa0c603bee29253ef917e4b078fe25ecb1ad09c
vetal0352 <vetal0352@gmail.com>
2021-07-07 21:15:07 +0300

    2021-07-07 add useEffect to start render Smile component

--- src/components/smile/Smile.jsx
+++ src/components/smile/Smile.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useEffect } from 'react'
 import { connect } from 'react-redux'
 import styles from "./Smile.module.css"
+import { acStart } from "../../store/store";
 
 const getLsim1Value = state => state.lsim1
 const getLsim2Value = state => state.lsim2
@@ -10,11 +12,11 @@ const getLsim5Value = state => state.lsim5
 
 const Smile = (props) => {
     debugger
+    useEffect(() => {props.start()})
     return (
       <div className={styles.smile}>
         <svg version="1.1" width="100%" height="100%">
           <rect x="0" y="0" width="350" height="200" rx="20"/>
-<<<<<<< HEAD
 
           {props.lsim1 
             ? <line x1="40" y1="30" x2="310" y2="30" style={{strokeDasharray: 15, stroke: "#ff0000", strokeWidth: 4+"px"}} />
@@ -26,12 +28,6 @@ const Smile = (props) => {
             : <line x1="30" y1="40" x2="320" y2="40" />
           }
           
-=======
-          { props.lsim1 ? <line x1="40" y1="30" x2="30" y2="30" />
-                        : <line x1="40" y1="30" x2="310" y2="30" />
-          }
-          <line x1="30" y1="40" x2="320" y2="40" />
->>>>>>> b4b83d54cf481c333443176d42666db97fe241df
           <line x1="50" y1="65" x2="130" y2="65" />
           <line x1="40" y1="55" x2="50" y2="65" />
           <line x1="220" y1="65" x2="300" y2="65" />
@@ -55,8 +51,12 @@ const Smile = (props) => {
     lsim5: getLsim5Value(state)
   })
   
+  const mapDispatchToProps = dispatch => ({
+    start: () => dispatch(acStart()),
+  })
+
   const SmileConnected = connect(
-    mapStateToProps
+    mapStateToProps, mapDispatchToProps
   )(Smile)
   
   export default SmileConnected;
